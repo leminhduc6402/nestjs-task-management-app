42becdff5aa543690f6c2ca80c5179f19d86fc48
leminhduc6402 <leminhduc6402@gmail.com>
2024-12-10 16:21:49 +0700

    Config and create schema user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
 
   controllers: [AppController],
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  active: boolean;
+
+  @Prop()
+  avatar: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  createdBy: string; // ref: user
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  updatedBy: string; // ref: user
+
+  @Prop()
+  deletedAt: Date;
+
+  @Prop()
+  deletedBy: string; // ref: user
+
+  @Prop()
+  isDeleted: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
